1666f47e9b1fa22dd51cabdcfbb4b780fca8d72d
Your Name <you@example.com>
2022-12-11 13:41:48 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,35 @@
+        function home()
+{
+    document.getElementById("extra").innerHTML=
+    "I am a Full time web Developer.<br>I am still studying 2<sup>nd</sup> Year of <span>BSC.IT</span>"
+}
+        function about()
+        {
+            document.getElementById("extra").innerHTML=
+    "I am studying in 2<sup>nd</sup>year of <span>IT</span> my age is 20 yrs.<br> I am always ready to learn the new things.<br>"
+        }
+        function service()
+{
+    document.getElementById("extra").innerHTML=
+    "Beginner Web Developer.<span> Freasher</span>"
+}
+        function resume()
+{
+    document.getElementById("extra").innerHTML=
+    'Here is my <span>Resume</span><br>Click on <a href="resume.pdf"><span>Download</span></a>'
+}
+        function work()
+{
+    document.getElementById("extra").innerHTML=
+    "<span>Fresher.</span>"
+}
+        function contact()
+{
+    document.getElementById("extra").innerHTML=
+    'E-mail id :- <b><span>k.shamim000797@gmail.com</span></b><br>My social media  <a href="#"><b><span>Instagram</span></b></a><br> My contact no. <b><span>9372144047</span></b>'
+}
+        function blog()
+        {
+            document.getElementById("extra").innerHTML=
+    '<a href="https://dimensionparadise.blogspot.com/2022/02/privacy-policy-demo.html"><span>Blog</span></a> You can read my blogs here.'
+        }
